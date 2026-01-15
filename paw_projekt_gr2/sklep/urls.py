6d9687e2e8a26a76a742c9f5f63037255edea424
Ilona Bedlińska <ibedlinska@st.swps.edu.pl>
2026-01-15 17:06:47 +0100

    Dodane dzialajace dodawanie i usuwanie przez formularz + widok, formularze html i stworzenie wizualnej czesci

--- paw_projekt_gr2/sklep/urls.py
+++ paw_projekt_gr2/sklep/urls.py
@@ -7,4 +7,7 @@ urlpatterns = [
 
     path('kategorie/', views.KategoriaListView.as_view(), name='kategoria-list'),
     path('kategorie/<int:pk>/', views.KategoriaDetailView.as_view(), name='kategoria-detail'),
+    path('html/produkty/', views.produkt_list_html, name='produkt-list-html'),
+    path('html/produkty/<int:id>/', views.produkt_detail_html, name='produkt-detail-html'),
+    path('html/produkty/dodaj/', views.produkt_create_html, name='produkt-create-html'),
 ]
--- paw_projekt_gr2/sklep/views.py
+++ paw_projekt_gr2/sklep/views.py
@@ -1,7 +1,7 @@
 from rest_framework import generics
 from .models import Produkt, Kategoria
 from .serializers import ProduktSerializer, KategoriaSerializer
-
+from django.shortcuts import render, redirect, get_object_or_404
 
 class ProduktListCreateView(generics.ListCreateAPIView):
     queryset = Produkt.objects.all()
@@ -20,3 +20,49 @@ class KategoriaListView(generics.ListCreateAPIView):
 class KategoriaDetailView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Kategoria.objects.all()
     serializer_class = KategoriaSerializer
+
+def produkt_list_html(request):
+    produkty = Produkt.objects.all()
+    return render(request, 
+                  "sklep/produkt/list.html", 
+                  {'produkty': produkty})
+
+
+
+def produkt_detail_html(request, id):
+    produkt = get_object_or_404(Produkt, id=id)
+
+    if request.method == "POST":
+        produkt.delete()
+        return redirect('produkt-list-html')
+
+    return render(request, 
+                  "sklep/produkt/detail.html", 
+                  {'produkt': produkt})
+
+
+
+
+def produkt_create_html(request):
+    kategorie = Kategoria.objects.all() 
+
+    if request.method == "POST":
+        nazwa = request.POST.get('nazwa')
+        opis = request.POST.get('opis')
+        cena = int(request.POST.get('cena'))
+        ilosc_z_formularza = request.POST.get('ilosc')
+        kategoria_id = request.POST.get('kategoria')
+        
+
+        if nazwa and cena and kategoria_id:
+            kat = Kategoria.objects.get(id=kategoria_id)
+            Produkt.objects.create(
+                nazwa=nazwa,
+                opis=opis,
+                cena=cena,
+                stan_magazynowy=ilosc_z_formularza,
+                kategoria=kat
+            )
+            return redirect('produkt-list-html')
+    
+    return render(request, "sklep/produkt/create.html", {'kategorie': kategorie})
